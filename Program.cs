7b73466acf52f04451ab02ff73d096aaae13ada4
program1519 <program1519xd@gmail.com>
2025-03-30 01:07:13 +0700

    yep

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       Stopwatch stopwatch = new Stopwatch();
+        Console.WriteLine("Enter to start!");
+        Console.ReadLine();
+        stopwatch.Start();
+
+
+  while (!Console.KeyAvailable || Console.ReadKey(true).Key != ConsoleKey.Enter)
+        {
+            Console.Clear();
+            TimeSpan ts = stopwatch.Elapsed;
+            Console.WriteLine("{0:00}:{1:00}:{2:00}.{3:00}",
+                ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
+            Thread.Sleep(100);
+        }
+		
+		
+        TimeSpan finalTime = stopwatch.Elapsed;
+        Console.Clear();
+        Console.WriteLine("END: {0:00}:{1:00}:{2:00}.{3:00}",
+            finalTime.Hours, finalTime.Minutes, finalTime.Seconds, finalTime.Milliseconds / 10);
+        Console.WriteLine("...");
+        Console.ReadLine();
+    }
+}
